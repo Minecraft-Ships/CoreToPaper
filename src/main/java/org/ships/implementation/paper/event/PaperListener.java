63d179d8e1df6069a26ab2b90c837795eb4c52dd
mosemister <nekronmoseley@gmail.com>
2021-04-09 20:59:46 +0100

    Ships Beta 10.3

--- src/main/java/org/ships/implementation/paper/event/PaperListener.java
+++ src/main/java/org/ships/implementation/paper/event/PaperListener.java
@@ -3,6 +3,7 @@ package org.ships.implementation.paper.event;
 import com.destroystokyo.paper.event.block.BlockDestroyEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.core.event.EventPriority;
 import org.core.world.position.block.details.BlockDetails;
 import org.core.world.position.impl.sync.SyncBlockPosition;
 import org.ships.implementation.bukkit.event.BukkitListener;
@@ -13,11 +14,11 @@ import org.ships.implementation.paper.event.events.block.PBlockChangeEvent;
 public class PaperListener implements Listener {
 
     @EventHandler
-    public void onBlockBreak(BlockDestroyEvent event){
+    public void onBlockBreak(BlockDestroyEvent event) {
         SyncBlockPosition position = new BBlockPosition(event.getBlock());
         BlockDetails details = new BBlockDetails(event.getNewState(), false);
         PBlockChangeEvent.BlockBreakEvent event2 = new PBlockChangeEvent.BlockBreakEvent(position, position.getBlockDetails(), details);
-        BukkitListener.call(event2);
+        BukkitListener.call(EventPriority.IGNORE, event2);
         event.setCancelled(event2.isCancelled());
     }
 }
